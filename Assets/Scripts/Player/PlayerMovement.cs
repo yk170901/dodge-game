9c9f9c97321e8f7b947f1adb86eaaeea69ea5c6c
yk170901 <yk170901@gmail.com>
2023-11-22 18:04:04 +0900

    I... did something? and i dont know what i did? I maybe separated a class file into two? it seems so?

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+    class PlayerMovement : MonoBehaviour
+    {
+        private Rigidbody _rb;
+
+        private Animator _animator;
+
+        private bool _isGameOver = false;
+
+        [SerializeField] private float _speed;
+        //private float _speed = 7f;
+
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody>();
+            _animator = GetComponent<Animator>();
+        }
+
+        private void Update()
+        {
+            if (_isGameOver) return;
+
+            Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+            _animator.SetBool("IsRunning", (moveDirection.x != 0 || moveDirection.z != 0));
+            _rb.velocity = moveDirection * _speed; // _speed를 PlayerData에서 받아와서 사용하는 걸로. 이래야 speedUp Item 먹었을 때 적용됨
+
+            if (moveDirection != Vector3.zero)
+                transform.forward = moveDirection;
+        }
+
+    }
+}
